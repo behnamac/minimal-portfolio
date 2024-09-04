46f4a86d40449094f419900938bd955e249dde77
Behnam <behnam.sep@gmail.com>
2024-09-04 12:24:47 +0200

    add hero component
    - set tailwind config
    -set spotlight effect
    -set grid effect component

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,11 @@
 import Image from "next/image";
+import { Hero } from "../components/Hero";
 
 export default function Home() {
   return (
-    <main className="relative bg-black-100">
+    <main className="relative bg-black-100 flex justify-center items-center flex-col overflow-hidden mx-auto sm:px-10 px-5">
       <div>
-        <h1>Hello Behnam</h1>
+        <Hero />
       </div>
     </main>
   );
